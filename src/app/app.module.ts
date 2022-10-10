05baf0f90fc59d44ebe1e1dc63e443bb96c8d197
Rodrigo Farias <iam.rodrigofarias@gmail.com>
2022-10-10 10:23:10 -0300

    commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { InlineStylesCspModule } from './inline-styles-csp/inline-styles-csp.module';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InlineStylesCspModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
